987771f9c0da1cba78ed0e490537674647e3ce1c
Pasindu Akalanka <pasinduakalanka08@gmail.com>
2024-07-19 11:36:52 +0530

    create components

---
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FoodItem.css'
+
+const FoodItem = ({id,name,price,description,image}) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default FoodItem

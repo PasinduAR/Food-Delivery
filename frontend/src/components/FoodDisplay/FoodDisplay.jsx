989e91b59976a2e3c4976d4cab9d08178a5b58a8
Pasindu Akalanka <pasinduakalanka08@gmail.com>
2024-07-20 23:36:37 +0530

    create add & remove button in food item component

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,18 +1,18 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
-import { food_list } from '../../assets/assets'
 import { StoreContext } from '../../Context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
 
 const FoodDisplay = ({category}) => {
 
     const { food_list } = useContext(StoreContext)
 
   return (
-    <div className='food-display' id='food-display'>
-      <h2>Top dishes near you</h2>
-      <div className="food-display-list">
+    <div className=' mt-[30px]' id='food-display'>
+      <h2 className=" text-responsive04 font-semibold ">Top dishes near you</h2>
+      <div className="grid grid-cols-auto-fill-minmax mt-[30px] gap-[30px] gap-y-[50px] ">
         {food_list.map((item, index) => {
-          return
+          return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
         })}
       </div>
     </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,12 +14,23 @@ export default {
         },
       },
       animation: {
-        fadeIn: 'fadeIn 3s ease-in-out',
+        fadeIn1: 'fadeIn 3s ease-in-out',
+        fadeIn2: 'fadeIn 1s',
       },
       fontSize: {
         'responsive01': 'max(4.5vw, 22px)',
         'responsive02': 'max(1vw, 13px)',
         'responsive03': 'max(1.4vw, 16px)',
+        'responsive04': 'max(2vw, 24px)',
+      },
+      gridTemplateColumns: {
+        'auto-fill-minmax': 'repeat(auto-fill, minmax(240px, 1fr))',
+      },
+      boxShadow: {
+        'custom': '0px 0px 10px #00000015',
+      },
+      borderRadius: {
+        'custom-tl-br': '15px 15px 0px 0px',
       },
     },
   },
